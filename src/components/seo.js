01e47b7808b8e89ca6bb864185be2ac0218e0eb5
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2022-02-24 14:12:18 -0500

    adding new script

--- src/components/seo.js
+++ src/components/seo.js
@@ -88,6 +88,25 @@ const Seo = ({ description, lang, meta, title }) => {
 }(window, document, 'ttq');`}
         </script>
       )}
+      {!!isBrowser && (
+        <script defer type="application/javascript">
+          {`<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '3618624564866369');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=3618624564866369&ev=PageView&noscript=1"
+/></noscript>`}
+        </script>
+      )}
     </Helmet>
   )
 }
