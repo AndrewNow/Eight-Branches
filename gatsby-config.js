669613f438e90ef5f2228664963ca5930357986a
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-01-09 18:24:32 -0500

    testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,15 +115,15 @@ module.exports = {
             // gatsby-remark-relative-images must go before gatsby-remark-images
             resolve: `gatsby-remark-relative-images`,
             options: {
-              // // [Optional] The root of "media_folder" in your config.yml
-              // // Defaults to "static"
-              // staticFolderName: "static",
-              // // [Optional] Include the following fields, use dot notation for nested fields
-              // // All fields are included by default
+              // [Optional] The root of "media_folder" in your config.yml
+              // Defaults to "static"
+              staticFolderName: "static",
+              // [Optional] Include the following fields, use dot notation for nested fields
+              // All fields are included by default
               // include: ["featured"],
-              // // [Optional] Exclude the following fields, use dot notation for nested fields
-              // // No fields are excluded by default
-              // exclude: ["featured.skip"],
+              // [Optional] Exclude the following fields, use dot notation for nested fields
+              // No fields are excluded by default
+              exclude: ["featured.skip"],
             },
           },
           {
